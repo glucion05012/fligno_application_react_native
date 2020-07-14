3e860a041da1e68719af19dd83c2baa5d2159015
Ágata Jiménez <giddel.lucion@yahoo.com>
2020-07-14 15:03:07 +0800

    added API components

--- src/components/api-component.js
+++ src/components/api-component.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Linking } from 'react-native';
 
 import { styles } from '../styles/styles.js'
+import { FlatList } from 'react-native-gesture-handler';
+import { ListItem } from 'react-native-elements';
+
+const list = [
+    {
+        name: 'YouTube Search API',
+        url: 'http://giddel-lucion-portfolio.herokuapp.com/projects/api',
+        subtitle: 'YouTube video search API connection sample using PHP'
+    },
+    {
+        name: 'Face Recognition Library',
+        url: 'https://face-recognition-js.herokuapp.com/',
+        subtitle: 'a JavaScript library to open camera of device to capture face expression.'
+    },
+    {
+        name: 'Facebook Messenger API',
+        url: 'http://giddel-lucion-portfolio.herokuapp.com/projects/facebook-messenger-api',
+        subtitle: 'Facebook messenger API to let you send a private message to a facebook page.'
+    },
+];
 
 export default class API extends Component {
-    render() {
+
+    renderRow({ item }) {
         return (
-            <View style={styles.center}>
-                <Text style={styles.title}>This is API page!</Text>
+            <ListItem
+                title={item.name}
+                subtitle={item.subtitle}
+                onPress={() => Linking.openURL(item.url)}
+            />
+        )
+    }
+
+    render() {
+
+        return (
+            <View>
+                <Text style={styles.header}> My API List </Text>
+
+                <FlatList
+                    data={list}
+                    renderItem={this.renderRow}
+                    keyExtractor={item => item.name}
+                />
             </View>
         );
     }
